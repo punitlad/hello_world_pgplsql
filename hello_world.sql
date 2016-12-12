e70dd4a1cd7c73002d13920ad313b28d217d669c
Punit Lad <punitlad@gmail.com>
2016-12-12 14:24:29 -0600

    added name variable

--- hello_world.sql
+++ hello_world.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION hello_world() RETURNS VARCHAR AS $$
 	v_name := 'Tommy';
 BEGIN
-		RETURN 'Hi New Agile World!';
+		RETURN 'Hi New Agile World!' || v_name;
 END; $$ LANGUAGE plpgsql;
